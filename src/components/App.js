e6afe55adf312d2615c319653a6f740964d75ef8
Zach Morse <zacharymorse@gmail.com>
2017-07-28 13:00:52 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import "../styles/App.css";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
